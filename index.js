e73d4d3f549e28928d8f85cf02ad76b44d4ed72c
Stefan Hyltoft <stefanhyltoft@gmail.com>
2018-07-26 20:07:30 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const request = require("request-promise");
+const cheerio = require("cheerio");
+const ObjectsToCsv = require("objects-to-csv");
+
+const url = "https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof";
+
+const scrapeSample = {
+  title: "Technical Autonomous Vehicle Trainer",
+  description:
+    "We're the driverless car company. We're building the world's best autonomous vehicles to safely connect people to the places, things, and experiences they care about.",
+  datePosted: new Date("2018-07-13"),
+  url:
+    "https://sfbay.craigslist.org/sfc/sof/d/technical-autonomous-vehicle/6642626746.html",
+  hood: "(SOMA / south beach)",
+  address: "1201 Bryant St.",
+  compensation: "23/hr"
+};
+
+const scrapeResults = [];
+
+async function scrapeJobHeader() {
+  try {
+    const htmlResult = await request.get(url);
+    const $ = await cheerio.load(htmlResult);
+
+    $(".result-info").each((index, element) => {
+      const resultTitle = $(element).children(".result-title");
+      const title = resultTitle.text();
+      const url = resultTitle.attr("href");
+      const datePosted = new Date(
+        $(element)
+          .children("time")
+          .attr("datetime")
+      );
+      const hood = $(element)
+        .find(".result-hood")
+        .text();
+      const scrapeResult = { title, url, datePosted, hood };
+      scrapeResults.push(scrapeResult);
+    });
+    return scrapeResults;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function scrapeDescription(jobsWithHeaders) {
+  return await Promise.all(
+    jobsWithHeaders.map(async job => {
+      try {
+        const htmlResult = await request.get(job.url);
+        const $ = await cheerio.load(htmlResult);
+        $(".print-qrcode-container").remove();
+        job.description = $("#postingbody").text();
+        job.address = $("div.mapaddress").text();
+        const compensationText = $(".attrgroup")
+          .children()
+          .first()
+          .text();
+        job.compensation = compensationText.replace("compensation: ", "");
+        return job;
+      } catch (error) {
+        console.error(error);
+      }
+    })
+  );
+}
+
+async function saveDataToCsv(data) {
+  const csv = new ObjectsToCsv(data);
+  await csv.toDisk("./craigslistjobs.csv");
+}
+
+async function scrapeCraigslist() {
+  const jobsWithHeaders = await scrapeJobHeader();
+  const jobsFullData = await scrapeDescription(jobsWithHeaders);
+  await saveDataToCsv(jobsWithHeaders);
+}
+
+scrapeCraigslist();
